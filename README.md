3e0acb2c8577ec49a7d80952a202ddd3a36dece9
rockitbaby <babe@rockitbaby.de>
2012-08-02 17:00:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,92 @@
+SVG Stacker
+===========
+
+An experimental tool that turns a folder of SVG files into a single SVG Stack.
+It also creates some demo HTML files that use the stack with different embedding-techniques (iframe, embed, img, object, background-image).
+
+SVG Stacker is based on NODE.js.
+
+The project contains two example stacks based icons of the famous [Tango Icon Library](http://tango.freedesktop.org/Tango_Icon_Library)
+and the popular [Iconic icon set by P.J. Onori](http://somerandomdude.com/work/iconic/).
+
+I created SVG Stacker to explore the power of the SVG Stack concept and test filesize, performance and browser-support.
+
+
+SVG Stacks
+-----------
+
+Simon 'Simurai' first described 'SVG Stacks' in March 2012. It's a technique to stack different SVG objects into one file to save HTTP Requests.
+
+> First we give each of our icons in the SVG file a unique ID and the same class name, add some CSS to hide all icons and only display the one that gets selected with :target. And now we can use a hash in the URL to pass the ID into the SVG file, like background: url(icon.svg#ID).
+
+[Read Simons blog post](http://simurai.com/post/20251013889/svg-stacks). Simon also created a [SVG Stack demo](http://jsfiddle.net/simurai/7GCGr/) that works pretty well in Firefox, but not yet in Webkit, see ([Webkit-Bug](https://bugs.webkit.org/show_bug.cgi?id=91790)).
+
+The ':target'-technique was first introduced by [@erikdahlstrom](https://twitter.com/erikdahlstrom) – it's worth checking out his [SVG Code snippets](http://xn--dahlstrm-t4a.net/svg/).
+
+
+Background
+-----------
+
+We decided to work with SVG for a recent mobile website project. It is an open standrad, the SVG format is [widly supported by browsers](http://caniuse.com/#search=svg) and leads to great visual results, especially when it comes to devices with different pixel densities.
+
+**"SVG-Stacks are the new Imagesprites." – Are they?**
+
+There are pros and cons. Here is a list of things we thought about so far:
+
+1.  Support for colors
+
+    compared to monocromatic icons, that can be easily embedded with Webfonts
+
+2.  Easier to write and _read_ in CSS
+
+    ```
+    button.cart {
+      background: transparent url(stack.svg#cart) no-repeat top left;
+    }
+    ```
+
+    compared to
+
+    ```
+    [data-icon]:before {
+        font-family: 'IcoMoon';
+        content: attr(data-icon);
+    }
+    <a href="#" class="button-with-icon" data-icon="+">Add</a>
+    ```
+
+    Webfont icon embed
+
+    Webfonts are great for monochrome icons. [Icomoon](http://keyamoon.com/icomoon/app/) is a great tool to define icon sets and add custom svg objects.
+     One could argue that they causes some semantic issues as they always produce content (compared to decorative background-image), which might be OK when using Unicodes [Private Use Area](http://en.wikipedia.org/wiki/Private_Use_(Unicode)).
+
+     But for Developers it might feel more natural to work with SVG Stacks as
+
+
+3.  SVG-Stacks behave like raster-images 
+
+    ```
+    <img src="stack.svg#flowers">
+    ```
+
+    They can be treat in HTML and CSS like we used to treat raster images.
+
+4.  Animation
+
+    See soccer-ball in [Example](examples/wikimedia/commons/stack/stack-demo-all.html)
+
+4.  Not yet available on Webkit
+
+    [Webkit-Bug](https://bugs.webkit.org/show_bug.cgi?id=91790)
+
+5.  Slow?
+
+    Test the examples: Some browser need an extensive rendering time.
+
+
+
+Related
+-----------
+
+* [Raphaël Icons](http://raphaeljs.com/icons/)
+* [Icomoon](http://keyamoon.com/icomoon/app/)
---
+++ index.js
@@ -0,0 +1,203 @@
+var fs = require('fs')
+  , path = require('path')
+  , easy = require('libxmljs-easy')
+  /* -- internal */
+  , stack = []
+  /* -- user */
+  , sourceDir = path.normalize('./')
+  , targetDir = null
+  ;
+
+function addToStack(file) {
+
+  var id = path.basename(file, '.svg');
+  var svgXml = fs.readFileSync(file, 'utf-8');
+  var svg = easy.parse(svgXml);
+
+  // manipulate svg
+  svg.$class = 'i';
+  svg.$id = id;
+
+  stack.push({
+    id: id,
+    svg: svg.$.toString().replace('xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"', ''),
+    w: svg.$width,
+    h: svg.$height
+  });
+}
+
+function outputSvg() {
+  
+  var svg = [];
+  svg.push('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">');
+
+  // @ see http://xn--dahlstrm-t4a.net/tmp/sharp-icons/svg-icon-target.svg
+  svg.push('<style type="text/css">');
+  svg.push('<![CDATA[');
+  svg.push('.i { display: none; fill: #010101; }');
+  svg.push('.i:target { display: block; }');
+  svg.push(']]>');
+  svg.push('</style>');
+
+  // need to make same ids from different files unique
+  stack.forEach(function(icon) {
+    svg.push(icon.svg);  
+  });
+  
+  svg.push('</svg>');  
+
+  var svgText = svg.join("\n");
+  
+  var filename = targetDir + '/stack.svg';
+  fs.writeFile(filename, svgText, function(err) {
+    if (err) throw err;
+    console.log("SVG Stack saved: " + filename);
+  });
+  
+}
+
+
+function readSourceDir(cb) {
+
+  console.log("Reading " + sourceDir);
+
+  fs.readdir(sourceDir, function(err, list) {
+    if (err) throw err;
+
+    var i = 0;
+    list.forEach(function(file) {
+      if(path.extname(file) != '.svg') {
+        return;
+      }
+      file = sourceDir + '/' + file;
+      addToStack(file);
+      i++;
+    });
+    if(i == 0) {
+      console.log("No SVG Files found in " + sourceDir);
+      console.log("svg-stacker SRC_DIR TARGET_DIR");
+    } else {
+      cb();
+    }
+  });
+}
+
+function htmlEscape(str) {
+    return String(str)
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+}
+
+function outputHTML(demo, name) {
+  var html = [];
+  html.push('<html><head><title>SVG Stack</title><style>body { font-family: Helvetica; } table { width: 100%; text-align: left; }</style></head>');
+  html.push('<body>');
+  html.push('<h1>SVG Stack</h1>');
+  html.push('<p>created with an early demo of https://github.com/preciousforever/svg-stacker</p>');
+  html.push('<table>');
+  html.push('<thead><tr>');
+  html.push('<th>Name</th>');
+  if(demo.object) {
+    html.push('<th>Object</th>');
+    html.push('<th>Code</th>');
+  }
+  if(demo.embed) {
+    html.push('<th>Embed</th>');
+    html.push('<th>Code</th>');
+  }
+  if(demo.iframe) {
+    html.push('<th>IFrame</th>');
+    html.push('<th>Code</th>');
+  }
+  if(demo.img) {
+    html.push('<th>Image</th>');
+    html.push('<th>Code</th>');
+  }
+  if(demo.background) {
+    html.push('<th>Background</th>');
+    html.push('<th>Code</th>');
+  }
+  html.push('</tr></thead>');
+  html.push('<tbody>');
+  stack.forEach(function(icon) {
+    
+    var url = 'stack.svg#' + icon.id;
+    var code = '';
+    html.push('<tr>');
+    html.push('<td>' + icon.id + '</td>');
+
+    if(demo.object) {
+      code = '<object type="image/svg+xml" data="' + url + '" width="' + icon.w + '" height="' + icon.h + '"></object>';
+      html.push('<td>' + code + '</td>');
+      html.push('<td><textarea cols="40" rows="3">' + htmlEscape(code) + '</textarea></td>');
+    }
+    if(demo.embed) {
+      code = '<embed src="' + url + '" type="image/svg+xml">';
+      html.push('<td>' + code + '</td>');
+      html.push('<td><textarea cols="40" rows="3">' + htmlEscape(code) + '</textarea></td>');
+    }
+    if(demo.iframe) {
+      code = '<iframe src="' + url + '" width="' + icon.w + '" height="' + icon.h + '"></iframe>';
+      html.push('<td>' + code + '</td>');
+      html.push('<td><textarea cols="40" rows="3">' + htmlEscape(code) + '</textarea></td>');
+    }
+    if(demo.img) {
+      code = '<img src="' + url + '" width="' + icon.w + '" height="' + icon.h + '">';
+      html.push('<td>' + code + '</td>');
+      html.push('<td><textarea cols="40" rows="3">' + htmlEscape(code) + '</textarea></td>');
+    }
+    if(demo.background) {
+      code = '<div style="background: transparent url(' + url + ') no-repeat top left; width: ' + icon.w + '; height: ' + icon.h + ';"></div>' 
+      html.push('<td>' + code + '</td>');
+      html.push('<td><textarea cols="40" rows="3">' + htmlEscape(code) + '</textarea></td>');
+    }
+    //html.push(icon.svg);
+    html.push('</tr>');
+    
+  });
+  html.push('</tbody>');
+  html.push('</body>');
+  html.push('</html>');
+  
+  var htmlText = html.join("\n");
+  
+  var filename = targetDir + '/stack-demo-' + name + '.html';
+  fs.writeFile(targetDir + '/stack-demo-' + name + '.html', htmlText, function(err) {
+    if (err) throw err;
+    console.log("HTML-Demo saved: " + filename);
+  });
+}
+
+
+// read command line arguments
+if(process.argv.length == 4) {
+  sourceDir = path.normalize(process.argv[2]);
+  targetDir = path.normalize(process.argv[3]);
+} else if (process.argv.length == 3) {
+  sourceDir = path.normalize(process.argv[2]);
+}
+
+if(targetDir == null) {
+  targetDir = sourceDir + '/stack';
+}
+
+// create target directory
+if (!path.existsSync(targetDir)) {
+  fs.mkdirSync(targetDir);
+}
+
+readSourceDir(function() {
+
+  outputSvg();
+
+  outputHTML({background: true}, 'background');
+  outputHTML({object: true}, 'object');
+  outputHTML({embed: true}, 'embed');
+  outputHTML({iframe: true}, 'iframe');
+  outputHTML({img: true}, 'img');
+  outputHTML({object: true, embed: true, iframe: true, background: true, img: true}, 'all');
+
+});
