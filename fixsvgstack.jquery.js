79e492bd7bea74745ed43eb1fcee69e721d67adc
Michael Schieben <babe@rockitbaby.de>
2013-02-25 11:30:40 +0100

    no need for base64 encoding
    as seen here: https://github.com/somerandomdude/SVG-Injector

--- fixsvgstack.jquery.js
+++ fixsvgstack.jquery.js
@@ -50,7 +50,7 @@
         // `parts[1]` contains id
         var svg = xml.getElementById(parts[1]);
         var svgString = (new XMLSerializer()).serializeToString(svg);
-        var dataURI = 'data:image/svg+xml;charset=utf-8;base64,' + Base64.encode(svgString);
+        var dataURI = 'data:image/svg+xml;utf-8,' + escape(svgString);
         cb(dataURI);
       });
   }
@@ -110,75 +110,5 @@
     });
     return this;
   };
-
-  // Helpers
-  // ----------
-
-  // Base64 module is used to create data urls
-  // 
-  // taken from
-  // http://www.webtoolkit.info/javascript-base64.html
-  var Base64 = {
-
-    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-    encode : function (input) {
-        var output = "";
-        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-        var i = 0;
-
-        input = Base64._utf8_encode(input);
-
-        while (i < input.length) {
-
-            chr1 = input.charCodeAt(i++);
-            chr2 = input.charCodeAt(i++);
-            chr3 = input.charCodeAt(i++);
-
-            enc1 = chr1 >> 2;
-            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-            enc4 = chr3 & 63;
-
-            if (isNaN(chr2)) {
-                enc3 = enc4 = 64;
-            } else if (isNaN(chr3)) {
-                enc4 = 64;
-            }
-
-            output = output +
-            this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-            this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
-
-        }
-
-        return output;
-    },
-
-    _utf8_encode : function (string) {
-        string = string.replace(/\r\n/g,"\n");
-        var utftext = "";
-
-        for (var n = 0; n < string.length; n++) {
-
-            var c = string.charCodeAt(n);
-
-            if (c < 128) {
-                utftext += String.fromCharCode(c);
-            }
-            else if((c > 127) && (c < 2048)) {
-                utftext += String.fromCharCode((c >> 6) | 192);
-                utftext += String.fromCharCode((c & 63) | 128);
-            }
-            else {
-                utftext += String.fromCharCode((c >> 12) | 224);
-                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                utftext += String.fromCharCode((c & 63) | 128);
-            }
-
-        }
-
-        return utftext;
-    }
-  }
 
 })(jQuery);
